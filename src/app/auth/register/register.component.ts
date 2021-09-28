d1a52b600de42f608402a2c72ec7a78fa698eb43
Fernando Rivera <ferchofercho933@gmail.com>
2021-09-27 21:50:54 -0500

    login and register funtional

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,13 +8,31 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  user = {
+    email:'',
+    password:'',
+    name:'',
+    lastname:''
+  }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+    ) { }
 
-  constructor( private router: Router) { }
-
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
-
+  signUp() {
+    this.authService.signUpUser(this.user)
+      .subscribe(
+        res => {
+          console.log(res);
+          localStorage.setItem('token', res.token);
+          this.router.navigate(['/auth/login']);
+        },
+        err => console.log(err)
+      )
+  }
 
   LoginNavigate(){
     this.router.navigate(['/auth/login']);
