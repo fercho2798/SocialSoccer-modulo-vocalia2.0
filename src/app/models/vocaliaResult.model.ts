8dcdcbc630510970226b39adafe03ed10ea91dd7
Michael9914 <michael_9914@hotmail.com>
2021-10-01 14:45:22 -0500

    add changes in model vocal result

--- src/app/models/vocaliaResult.model.ts
+++ src/app/models/vocaliaResult.model.ts
@@ -1,10 +1,12 @@
 export class VocalResult {
-  constructor(_id = "", team1 = "", team2 = "", CardsRed = 0, CardsYellow = 0, goals1 = 0,  goals2 = 0, changes1 = 0, changes2 = 0, observation='' ) {
+  constructor(_id = "", team1 = "", team2 = "", CardsRed1 = 0, CardsRed2 = 0, CardsYellow1 = 0, CardsYellow2 = 0, goals1 = 0,  goals2 = 0, changes1 = 0, changes2 = 0, observation='' ) {
       this._id = _id;
       this.team1 = team1;
       this.team2 = team2;
-      this.CardsRed = CardsRed;
-      this.CardsYellow = CardsYellow;
+      this.CardsRed1 = CardsRed1;
+      this.CardsRed2 = CardsRed2;
+      this.CardsYellow1 = CardsYellow1;
+      this.CardsYellow2 = CardsYellow2;
       this.goals1 = goals1;
       this.goals2 = goals2;
       this.changes1 = changes1;
@@ -16,8 +18,10 @@ export class VocalResult {
   _id:string;
   team1:string;
   team2:string;
-  CardsRed:number;
-  CardsYellow:number;
+  CardsRed1:number;
+  CardsRed2:number;
+  CardsYellow1:number;
+  CardsYellow2:number;
   goals1:number;
   goals2: number;
   changes1: number;
